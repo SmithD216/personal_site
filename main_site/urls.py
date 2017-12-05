cdaa4fa67d27b96909fae6aaeb63fd47a1dda983
Dylan Smith <smithd216@protonmail.com>
2017-12-05 22:36:05 +0000

    Resolving urlpattern namespace issues.

--- main_site/urls.py
+++ main_site/urls.py
@@ -1,8 +1,10 @@
-"""Defines URL patterns for the main_site"""
+"""Defines URL patterns for the main_site."""
 
 from django.conf.urls import url
 
 from . import views
+
+app_name = 'main_site'
 
 urlpatterns = [
     # Home page
