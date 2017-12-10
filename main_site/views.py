6d829b9967d5bbab95cb531e6c0586a181b6373e
Dylan Smith <smithd216@protonmail.com>
2017-12-10 22:38:51 +0000

    Updated the index page to list entries.

--- main_site/views.py
+++ main_site/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+from .models import Entry
 # Create your views here.
 
 def index(request):
     """The home page for the site."""
-    return render(request, 'main_site/index.html')
+    entries = Entry.objects.order_by('-date_added')
+    context = {'entries':entries}
+    return render(request, 'main_site/index.html', context)
