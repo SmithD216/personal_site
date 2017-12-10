0f200632cc9bf78033410e0c5d15ffa36b83f8ce
Dylan Smith <smithd216@protonmail.com>
2017-12-10 22:12:06 +0000

    Added Entry model.

--- main_site/models.py
+++ main_site/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Entry(models.Model):
+    """A post on the website."""
+    date_added = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=108)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text[:50] + "..."
