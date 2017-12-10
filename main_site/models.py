6176a63449551731e3453b65f32508fdeed33684
Dylan Smith <smithd216@protonmail.com>
2017-12-10 22:25:05 +0000

    Updated Entry model and admin site urls.

--- main_site/models.py
+++ main_site/models.py
@@ -6,6 +6,7 @@ class Entry(models.Model):
     """A post on the website."""
     date_added = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=108)
+    text = models.TextField()
 
     class Meta:
         verbose_name_plural = 'entries'
